b503064385801c781e87bbce13627687d0533751
Neuheit <38368299+Neuheit@users.noreply.github.com>
2020-12-06 16:55:05 -0500

    Create consumer.c

---
+++ consumer.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+
+int main() {
+
+    // Allocated shared memory and semaphores.
+
+    int shm_fd = shm_open("table", O_RDWR, 0666);
+    int* table = mmap(0,sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    sem_t* fill = sem_open("fill", O_CREAT,0666,0);
+    sem_t* avail = sem_open("available", O_CREAT, 0666, 2);
+    sem_t* mutex = sem_open("mutex", O_CREAT,0666,1);
+
+    // Here we wait until the producer produces
+    // an item, wait a random number of ms, 
+    // use mutual exclusion to decrement the items
+    // and signal the available semaphore, 
+    // which the producer will wait on.
+
+    int loop = 6;  
+    printf("\nConsumer ready to receive %d items.\n", loop);
+
+    for(int i = 1; i < loop; ++i) {
+        sem_wait(fill);
+        
+        int ms = rand() % 2 + 1;
+        sleep(ms);
+
+        sem_wait(mutex);
+        --(*table);
+        sem_post(mutex);
+
+        printf("Item consumed, %d remaining.\n", *table);
+
+        sem_post(avail);
+    }
+
+    // Close and unlink semaphores
+    // Deallocate shared memory
+
+    sem_close(fill);
+    sem_close(avail);
+    sem_close(mutex);
+
+    sem_unlink("fill");
+    sem_unlink("available");
+    sem_unlink("mutex");
+
+    munmap(table, sizeof(int));
+    close(shm_fd);
+    shm_unlink("table");
+
+    printf("Consumer cleaned up!\n");
+    return 0;
+}
